5276d28eba8e8662131c1d97e73f18315e470fb2
Naveen Pal <paln76050@gmail.com>
2025-03-11 13:15:00 +0530

    Update session_utils.py

--- app/utils/session_utils.py
+++ app/utils/session_utils.py
@@ -1,7 +1,5 @@
 from app.models.session import Session
 
 def verify_session(session_id, user_type):
-    print(session_id,user_type)
     session = Session.query.filter_by(session_id=session_id, user_type=user_type, is_active=1).first()
-    print(" what is session"," ",session)
     return session is not None
