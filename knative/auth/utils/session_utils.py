c517131a4efc7f035003c4e90a32f57abfa7b0ef
Naveen-Pal <paln76050@gmail.com>
2025-03-28 02:46:13 +0530

    sync all services

--- knative/auth/utils/session_utils.py
+++ knative/auth/utils/session_utils.py
@@ -1,4 +1,4 @@
-from models.session import Session
+from app.models.session import Session
 
 def verify_session(session_id, user_type):
     session = Session.query.filter_by(session_id=session_id, user_type=user_type, is_active=1).first()
