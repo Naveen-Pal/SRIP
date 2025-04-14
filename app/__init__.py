d53441255b3339ac27b83662ccb0c115333dd3d0
Lakshya Kesarwani <ansh1.kesari2005@gmail.com>
2025-04-14 16:39:10 +0530

    refactor: remove redundant import of JWTManager in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,6 @@ from flask_jwt_extended import JWTManager
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
-from flask_jwt_extended import JWTManager
-import os 
 
 db = SQLAlchemy()
 jwt = JWTManager()
